72895a4a678e13af6d058c567d0ebac4d934e9df
wyattwas <wyatt.ruehl@mailbox.org>
2025-01-30 20:05:37 +0100

    fix nav links

--- components/nav.php
+++ components/nav.php
@@ -7,10 +7,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">Start <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php">Start <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="contact.htnl">Kontakt</a>
+                <a class="nav-link" href="contact.php">Kontakt</a>
             </li>
         </ul>
     </div>

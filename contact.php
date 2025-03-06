99fb24354a62fc1252dc96d442626d100947546b
wyattwas <wyatt.ruehl@mailbox.org>
2025-03-06 15:54:38 +0100

    adding content

--- contact.php
+++ contact.php
@@ -5,14 +5,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Kontakt | Sippis @ DPSG Idstein</title>
     <link rel="icon" type="image/png" href="img/rochen_logo-circle.png">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <meta name="description" content="Die Roverrunde des DPSG Sankt Martin Idstein">
     <meta name="theme-color" content="#ff4d4d">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous"></script>
 </head>
 <body>
-<?php include "components/nav.php" ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><img src="img/rochen_logo-circle.png" alt="Sippis" style="max-height: 100px"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Start <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="contact.php">Kontakt</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="content">
+    <div>
+        <p>
+            Wir sind die Roverrunde "Sippis", des DPSG Stamms Sankt Martin Idstein.
+            Zusammen sind wir 7 Rover.<br>
+            Unsere Gruppenstunden finden jeden Mittwoch von 18:00 Uhr bis 19:30 Uhr, im Gemeindehaus der katholischen
+            Gemeinde Idstein statt.
+        </p>
+        <p>
+            Erreichen könnt ihr uns hier:<br>
+            E-Mail: <a href="mailto:kontakt@sippis-dpsg.de">kontakt@sippis-dpsg.de</a><br>
+            Telefon: <a href="tel:+4917655233953">+49 176 55233953</a><br>
+        </p>
+        <p>
+            DPSG Stamm Sankt Martin Idstein<br>
+            Wiesbadener Straße 21-31<br>
+            65510 Idstein
+            <br>
+            <iframe width="425" height="350"
+                    src="https://www.openstreetmap.org/export/embed.html?bbox=8.2647442817688%2C50.21820046084595%2C8.268284797668459%2C50.21970559126617&amp;layer=mapnik&amp;marker=50.21895303199209%2C8.266514539718628"
+                    style="border: none"></iframe>
+            <br>
+            <small>
+                <a href="https://www.openstreetmap.org/?mlat=50.218953&amp;mlon=8.266515#map=19/50.218953/8.266515">View
+                    Larger Map</a>
+            </small>
+        </p>
+    </div>
+</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Sippis @ DPSG Idstein</title>
     <link rel="icon" type="image/png" href="img/rochen_logo-circle.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
     <meta name="description" content="Die Roverrunde des DPSG Sankt Martin Idstein">
     <meta name="theme-color" content="#ff4d4d">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -13,6 +14,41 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
 <body>
-<?php include "components/nav.php" ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><img src="img/rochen_logo-circle.png" alt="Sippis" style="max-height: 100px"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Start <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact.php">Kontakt</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="content">
+    <div class="page page-canada">
+        <h1>Rover auf großer Fahrt</h1>
+        <p>
+            2026 wollen wir unsere Abschlussfahrt veranstalten.<br>
+            Unser Ziel ist Kanada. Da wir dafür noch jede Menge finanzielle Unterstützund benötigen,
+            bieten wir unsere Hilfe an.<br>
+            Wir können für euch Aufgaben übernehemen und würden uns über eine Spende freuen.
+            Z.B. Gartenarbeit, Einkäufe, Hilfe beim Umzug, Computerhilfe, oder Ähnliches<br>
+        </p>
+        <p>
+            Wenn ihr etwas habt, bei dem ihr Hilfe braucht, meldet euch gerne bei uns.<br>
+            <br>
+            E-Mail: <a href="mailto:kontakt@sippis-dpsg.de">kontakt@sippis-dpsg.de</a><br>
+            Telefon: <a href="tel:+4917655233953">+49 176 55233953</a>
+        </p>
+    </div>
+</div>
 </body>
 </html>

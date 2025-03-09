8c162f13a7b914e2f9a3399f3610ec1165144c1b
wyattwas <wyatt.ruehl@mailbox.org>
2025-03-09 21:50:50 +0100

    adding info about first job

--- index.php
+++ index.php
@@ -5,19 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sippis @ DPSG Idstein</title>
     <link rel="icon" type="image/png" href="img/rochen_logo-circle.png">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/style.css">
     <meta name="description" content="Die Roverrunde des DPSG Sankt Martin Idstein">
     <meta name="theme-color" content="#ff4d4d">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous"></script>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#"><img src="img/rochen_logo-circle.png" alt="Sippis" style="max-height: 100px"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
@@ -50,8 +58,33 @@
             Telefon: <a href="tel:+4917655233953">+49 176 55233953</a>
         </p>
         <p>
-            <img src="img/Rover-auf-großer-Fahrt.png" alt="Rover auf großer Fahrt" style="width: 100%; max-width: 425px;">
+            <img src="img/Rover-auf-großer-Fahrt.png" alt="Rover auf großer Fahrt"
+                 style="width: 100%; max-width: 425px; border-radius: 8px; border: 1px solid #bdbdbd">
         </p>
+    </div>
+    <div class="page page-canada">
+        <h1>Einen Schritt näher</h1>
+        <p>
+            Am 08.03. haben wir unseren ersten Auftrag absolviert.<br>
+            von 11 Uhr bis 17 Uhr haben wir mit unserem Stammesanhänger Grünschnitt und Renovierungsmüll zum Grünschnitt
+            und zur Deponie gefahren.<br>
+            Insgesamt sechs Hänger Ladungen haben wir bewegt.<br>
+        </p>
+        <p>
+            <a href="https://www.instagram.com/p/DG8yxo9OhE1/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+               style="text-decoration: none; border-radius: 8px; background-color: #007bff; text-align: center; color: white; width: 100%; display: block; font-size: 1.125rem; font-weight: 500; padding: 0.75rem 1.5rem;">Instagram
+                Post</a>
+        </p>
+        <div style="display: flex; flex-direction: column; max-width: inherit">
+            <img src="https://dpsg-idstein.de/images/Rover/IMG_20250308_160408973.jpg"
+                 alt="Drei Sippis bringen Grünschnitt weg" style="border-radius: 8px; margin: 3px"/>
+            <div style="display: flex; flex-direction: row">
+                <img src="https://dpsg-idstein.de/images/Rover/IMG_20250308_153946165.jpg" alt="Ein Sippi zägt Holz"
+                     style="border-radius: 8px; width: 50%; margin: 3px"/>
+                <img src="https://dpsg-idstein.de/images/Rover/IMG_20250308_160344226.jpg"
+                     alt="Unser vollbeladener Stammesanhänger" style="border-radius: 8px; width: 50%; margin: 3px"/>
+            </div>
+        </div>
     </div>
 </div>
 </body>

4949e2d6c5e10a1a0fc73c032db17252745d38e4
wyattwas <wyatt.ruehl@mailbox.org>
2026-03-08 13:21:58 +0100

    moving rosskur2026 in separate folder for usage as document root for rosskur2026.de

--- rosskur2026/index.php
+++ rosskur2026/index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sippis @ DPSG Idstein</title>
-    <link rel="icon" type="image/png" href="assets/rochen_logo-circle.png">
+    <link rel="icon" type="image/png" href="../assets/rochen_logo-circle.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <meta name="description" content="Die Roverrunde des DPSG Sankt Martin Idstein">
     <meta name="theme-color" content="#ff4d4d">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
@@ -22,8 +22,8 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php"><img src="assets/rochen_logo-circle.png" alt="Sippis"
-                                                  style="max-height: 100px"></a>
+    <a class="navbar-brand" href="../index.php"><img src="../assets/rochen_logo-circle.png" alt="Sippis"
+                                                     style="max-height: 100px"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -32,16 +32,16 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="index.php">Start</a>
+                <a class="nav-link" href="../index.php">Start</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="canada.php">Kanada 2026</span></a>
+                <a class="nav-link" href="../canada.php">Kanada 2026</span></a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link" href="rosskur2026.php">Rosskur 2026 <span class="sr-only">(current)</a>
+                <a class="nav-link" href="index.php">Rosskur 2026<span class="sr-only">(current)</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="contact.php">Kontakt</a>
+                <a class="nav-link" href="../contact.php">Kontakt</a>
             </li>
         </ul>
     </div>
@@ -51,7 +51,7 @@
     <div class="page">
         <h1>Rosskur 2026</h1>
         <p>
-            <img src="assets/Save_the_date_Rosskur_26.png" alt="Flyer Rosskur 2026"
+            <img src="../assets/Save_the_date_Rosskur_26.png" alt="Flyer Rosskur 2026"
                  class="w-100 border-radius-8 gray-border">
         </p>
         <p>
@@ -63,7 +63,7 @@
             Die Anmeldung und weitere Informationen folgen. Um nichts zu verpassen, folgt uns gerne auf Insta.
         </p>
         <p>
-            <a href="assets/rosskur.ics"
+            <a href="../assets/rosskur.ics"
                class="link-button text-white bg-danger">In den Kalender eintragen</a>
         </p>
         <p>
